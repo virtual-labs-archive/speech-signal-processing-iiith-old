c77b748d14ac9e06eeb02ea3ceabcfb4969432ad
Amit Singh <amitcogsci@gmail.com>
2019-06-21 14:39:34 +0530

    added residue plotting

--- SRIP/media/sketch_unvoiced.js
+++ SRIP/media/sketch_unvoiced.js
@@ -1,5 +1,6 @@
 var button;
-var wave;
+var rawplot;
+var residueplot;
 
 function preload(){
     sound = loadSound("/media/unvoiced.wav");
@@ -7,40 +8,76 @@ function preload(){
 
   
   function setup(){
-    cnv = createCanvas(600,200);
-    background(0);
-    button = createButton('generate')
-    button.mouseClicked(togglePlay)
-    textSize(10);
-  fill(255, 255, 255);
-  text('amplitude values along the time domain for unvoiced sound – Letter “p” in the word “please”' , 10, 12);
-  spectrum = new p5.FFT();
+  createCanvas(800,200);
+  button = createButton('generate')
+  button.mouseClicked(togglePlay)
+  textSize(10);
+  fill(255,0,0);
+  text('amplitude values along the time domain for Voiced sound – Letter “p” in the word “please”', 10, 12);
+  fill(0,0,0);
+  text('residual signal of the unvoiced sound', 500, 12);
+  spectrum = new p5.FFT(0,512);
+    
+  
+    
   }
    
   
   function draw(){
+  
+  drawrawplot();
+  drawresidueplot();
+  
+}    
 
+
+function drawrawplot() {
     beginShape();
     noFill();
     stroke(255,0,0); // waveform is red
     strokeWeight(1);
-    wave = spectrum.waveform();
+    waveform = spectrum.waveform();
 
-  for (var i = 0; i< wave.length; i++){
-    var x = map(i, 0, wave.length, 0, width);
-     var y = map( wave[i], 1, -1, 0, height);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, 390);
+    var y = map( waveform[i], 1, -1, 0, height);
   
     vertex(x,y);
     
     //console.log(waveform[i])
 
   }
-  
-  
   endShape();
-} 
+}
+
+function drawresidueplot() {
+  beginShape();
+    noFill();
+    stroke(0,0,0); // waveform is black
+    strokeWeight(1);
+    wave = spectrum.waveform();
+    //console.log(wave)
+
+  for (var i = 0; i< wave.length; i++){
+    var x = map(i, 0, wave.length, 410, 800);
+    var new_signal = wave[i-1]
+    var new_signal2 = new_signal + wave[i];
+    var y = map(new_signal2, 1, -1, 0, height);
+  
+    vertex(x,y);
+    
+    
+
+  }
+  endShape();
+  
+}
 
 
+
+ 
+
+//====================================================================
   function togglePlay() {
     if (sound.isPlaying()) {
       sound.pause();
@@ -50,3 +87,4 @@ function preload(){
     
     }
   }
+  
